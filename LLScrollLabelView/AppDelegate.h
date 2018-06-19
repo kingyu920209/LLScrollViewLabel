050a484676d41a8e930b99d76a707d54eda71791
嘚嘚以嘚嘚 <kingyu920209@126.com>
2018-06-19 14:44:30 +0800

    Initial Commit

---
+++ LLScrollLabelView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLScrollLabelView
+//
+//  Created by 嘚嘚以嘚嘚 on 2018/6/19.
+//  Copyright © 2018年 嘚嘚以嘚嘚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
